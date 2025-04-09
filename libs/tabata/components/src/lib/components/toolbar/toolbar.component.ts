d639fa5ccad513bf70d900cad12dfe179a3b5a6d
LAPTOP-ROIEOSRK\vtsitlak <vtsitlak@gmail.com>
2025-04-09 18:27:19 +0200

    fix popover

--- libs/tabata/components/src/lib/components/toolbar/toolbar.component.ts
+++ libs/tabata/components/src/lib/components/toolbar/toolbar.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject, input } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject, input, AfterViewChecked } from "@angular/core";
+import { CommonModule } from "@angular/common";
 import {
   IonButton,
   IonButtons,
@@ -12,13 +12,13 @@ import {
   IonPopover,
   IonTitle,
   IonToolbar,
-} from '@ionic/angular/standalone';
-import {  AuthStore } from '@silver/tabata/auth';
-import { addIcons } from 'ionicons';
-import { logOut, home, person, menu } from 'ionicons/icons';
-import { Router, RouterLink } from '@angular/router';
+} from "@ionic/angular/standalone";
+import { AuthStore } from "@silver/tabata/auth";
+import { addIcons } from "ionicons";
+import { logOut, home, person, menu } from "ionicons/icons";
+import { Router, RouterLink } from "@angular/router";
 @Component({
-  selector: 'tbt-toolbar',
+  selector: "tbt-toolbar",
   imports: [
     CommonModule,
     IonButtons,
@@ -34,20 +34,51 @@ import { Router, RouterLink } from '@angular/router';
     IonHeader,
     RouterLink,
   ],
-  templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.scss',
+  templateUrl: "./toolbar.component.html",
+  styleUrl: "./toolbar.component.scss",
   standalone: true,
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements AfterViewChecked {
+
   constructor() {
     addIcons({ home, menu, person, logOut });
   }
-  title = input<string | null>(null);
 
+  title = input<string | null>(null);
   router = inject(Router);
   private readonly authStore = inject(AuthStore);
+   // Track whether popover is open or not
+   showPopover = false;
+
+   // Toggle popover visibility
+   togglePopover(event: Event): void {
+    event.stopPropagation(); // Prevent click event propagation to avoid closing the popover when clicking inside
+    this.showPopover = !this.showPopover;
+  }
+
+    // Action after clicking on the menu item
+    onMenuItemClick(): void {
+      this.closePopover(); // Close popover after menu item is clicked
+    }
+
+  // Close the popover after it is dismissed
+  closePopover(): void {
+    this.showPopover = false;
+  }
 
   logout(): void {
     this.authStore.logout();
+    this.closePopover();
   }
+
+    // Ensure popover position recalculates after it's shown again
+    ngAfterViewChecked(): void {
+      if (this.showPopover) {
+        // Manually trigger popover position recalculation
+        const popover = document.querySelector('ion-popover');
+        if (popover) {
+          popover.style.position = 'absolute'; // Reset the position property
+        }
+      }
+    }
 }
