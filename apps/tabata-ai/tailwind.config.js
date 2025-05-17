58fe27d04d9d62a9f06e78d7e6b4b1dabd255092
LAPTOP-ROIEOSRK\vtsitlak <vtsitlak@gmail.com>
2025-05-17 17:36:54 +0200

    theming WIP

--- apps/tabata-ai/tailwind.config.js
+++ apps/tabata-ai/tailwind.config.js
@@ -5,8 +5,18 @@ module.exports = {
     content: [join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'), ...createGlobPatternsForDependencies(__dirname)],
     theme: {
         extend: {
+            fontFamily: {
+                sans: ['Figtree', 'Roboto', 'sans-serif'],
+            },
             backgroundImage: {
                 logo: "url('assets/logo-background.png')"
+            },
+            colors: {
+                primary: 'rgb(var(--color-primary) / <alpha-value>)',
+                'primary-dark': 'rgb(var(--color-primary-dark) / <alpha-value>)',
+                secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+                'secondary-dark': 'rgb(var(--color-secondary-dark) / <alpha-value>)',
+                error: 'rgb(var(--color-error) / <alpha-value>)',
             }
         }
     },
