85968225975222aa2fc33aa0062e43ca5baff768
LAPTOP-ROIEOSRK\vtsitlak <vtsitlak@gmail.com>
2026-03-11 11:48:55 +0100

    lint fixes

--- apps/tabata-ai/eslint.config.mjs
+++ apps/tabata-ai/eslint.config.mjs
@@ -6,6 +6,9 @@ export default [
   ...nx.configs['flat/angular'],
   ...nx.configs['flat/angular-template'],
   {
+    ignores: ['**/android/**', '**/build/**', '**/dist/**'],
+  },
+  {
     files: ['**/*.ts'],
     rules: {
       '@angular-eslint/directive-selector': [
